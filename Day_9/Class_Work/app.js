1e09da29ac1671f03ca306c3e5bf07e5fcfba569
mdramjankhan <rk9032929@gmail.com>
2025-02-24 11:36:32 +0530

    Adding the class work

---
+++ Day_9/Class_Work/app.js
@@ -0,0 +1,18 @@
+console.log(myName);
+myName();
+console.log(a);
+var a=10;
+console.log(a);
+
+function myName() {
+    console.log(a);
+}
+
+let fname = "Sudip";
+let lname = "Pradhan";
+
+console.log(fname, lname);
+
+let b = 20 ; let c = "30";
+
+console.log(b+c);
