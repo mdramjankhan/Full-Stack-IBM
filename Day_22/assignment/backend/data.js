2da743fb69d3497f933d92088ef5f504f58ef918
mdramjankhan <rk9032929@gmail.com>
2025-03-13 11:54:44 +0530

    add

--- Day_22/assignment/backend/data.js
+++ Day_22/assignment/backend/data.js
@@ -10,5 +10,17 @@ module.exports = [
         name: "Pants",
         price: 700,
         category: "Clothings"
+    },
+    {
+        id: "3",
+        name: "Iphone",
+        price: 70000,
+        category: "Electronics"
+    },
+    {
+        id: "4",
+        name: "Toys",
+        price: 200,
+        category: "Kids Stuffs"
     }
 ];
